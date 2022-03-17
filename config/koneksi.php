26033e6cbf97398b93f9cb37814fcd45d0f6c0e6
MRafly7421 <76813147+MRafly7421@users.noreply.github.com>
2022-03-17 19:02:02 +0700

    Add files via upload

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,6 +2,6 @@
     $host = "localhost";
     $user = "root";
     $pass = "";
-    $dbName = "db_farmasi_app_primhus";
+    $dbName = "db_farmasy_app_primhus";
     $koneksi = mysqli_connect($host,$user,$pass,$dbName); 
 ?>
