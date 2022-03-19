197872829be09c540c498becac6a88fd1c0d2db9
MRafly7421 <76813147+MRafly7421@users.noreply.github.com>
2022-03-19 13:16:21 +0700

    Delete config directory

--- config/action_edit_obat_masuk.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header("Location: ../admin/obat_masuk/cek_obat_masuk.php");
-?>
--- config/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $dbName = "db_farmasy_app_primhus";
-    $koneksi = mysqli_connect($host,$user,$pass,$dbName); 
-?>
